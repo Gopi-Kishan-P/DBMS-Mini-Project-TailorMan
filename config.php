919337e4fee9d49594ae0d2a47d77d34fe3638c1
Gopi Kishan P <gopi1792000@gmail.com>
2020-12-23 21:42:16 +0530

    create measurement table

--- config.php
+++ config.php
@@ -15,8 +15,14 @@
         $sql = "Use Tailorman;";
         mysqli_query($conn, $sql);
 
+        // CUSTOMER Table
         $sql = "CREATE TABLE IF NOT EXISTS `tailorman`.`customer` (`Cust_ID` INT(3) NOT NULL AUTO_INCREMENT UNIQUE, `Cust_Name` VARCHAR(30) NOT NULL , `Cust_Phno` VARCHAR(15) NOT NULL UNIQUE, `Cust_Password` VARCHAR(255) NOT NULL , PRIMARY KEY (`Cust_ID`, `Cust_Phno`))";
-
         mysqli_query($conn, $sql);
+        
+        // Measurement Table
+        $sql = "CREATE TABLE IF NOT EXISTS `tailorman`.`measurements` ( `Cust_ID` INT(3) NOT NULL, Foreign Key (Cust_Id) References `customer`(Cust_ID), `Shirt Length` FLOAT, `Shoulder` FLOAT, `Sleeves` FLOAT, `Chest` FLOAT, `Waist` FLOAT, `Hip` FLOAT, `Collar` FLOAT, `Pant Length` FLOAT, `Fork Round` FLOAT, `Thigh` FLOAT, `Knee` FLOAT, `Bottom` FLOAT )";
+        mysqli_query($conn, $sql);
+        
+        
     }
 ?>
