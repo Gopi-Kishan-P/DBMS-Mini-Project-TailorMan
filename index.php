a22e039d989fbfbe7393956a04e05464da80a80a
Gopi Kishan P <gopi1792000@gmail.com>
2020-12-24 09:45:55 +0530

    changed href to / from index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <!-- navigation bar -->
       <nav class="navbar navbar-expand-lg navbar-light cc-navbar p-0">
          <div class="container-fluid">
-            <a class="navbar-brand cc-nav-logo" href="index.php">TailorMan</a>
+            <a class="navbar-brand cc-nav-logo" href="/">TailorMan</a>
             <button class="navbar-toggler p-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="cc-menu">
                   <img src="images/menu.svg" alt="" width="30px" id="menu" onclick="change()">
@@ -37,7 +37,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav ml-auto text-right p-0 font-weigth-bolder">
                   <li class="nav-item">
-                     <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                   </li>
                   <!-- <li class="nav-item">
                      <a class="nav-link" href="#">Catalog</a>
--- login.php
+++ login.php
@@ -81,7 +81,7 @@ function login($conn, $phno, $passwd)
       <!-- navigation bar -->
       <nav class="navbar navbar-expand-lg navbar-light cc-navbar p-0">
          <div class="container-fluid">
-            <a class="navbar-brand cc-nav-logo" href="index.php">TailorMan</a>
+            <a class="navbar-brand cc-nav-logo" href="/">TailorMan</a>
             <button class="navbar-toggler p-0" type="button" data-toggle="collapse"
                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                aria-label="Toggle navigation">
@@ -92,7 +92,7 @@ function login($conn, $phno, $passwd)
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav ml-auto text-right p-0 font-weigth-bolder">
                   <li class="nav-item">
-                     <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                     <a class="nav-link" aria-current="page" href="/">Home</a>
                   </li>
                   <!-- <li class="nav-item">
                      <a class="nav-link" href="#">Catalog</a>
